0ba58137b631ef2521ead0f39ee73db271b5eb1a
ogawatti <ogawattim@gmail.com>
2022-09-02 09:52:39 +0900

    replace search to instant

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+import FormController from './form_controller'
+application.register('form', FormController)
